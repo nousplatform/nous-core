0e8af6580e20dce6b4a03c23d49026015ac1b423
dshm <dima.shmalko@gmail.com>
2018-05-31 11:30:28 +0300

    clean truffle config

--- truffle-config.js
+++ truffle-config.js
@@ -1,14 +1,11 @@
 module.exports = {
   networks: {
     development: {
-      //host: "192.168.88.13",
       host: "localhost",
       port: 8545,
       network_id: "*", // Match any network id
-      gas:   4700000,
-      gasPrice: 50000000000,
-      //from: "0x0b8976a4871ff9b0f1a33dee9c0ede304c0fa131"
-      //from: "0xc2Ddda15f8deB297EB4DE39B82ae0750dF693253",
+      gas: 4700000,
+      gasPrice: 50000000000
     },
     ropsten: {
       host: "192.168.88.13",
@@ -16,28 +13,24 @@ module.exports = {
       network_id: "3",
       gas: 4700000, // Gas limit used for deploys
       gasPrice: 50000000000, // 20 gwei
-      from: "0x26E196dbdE4d6cFA212fd5447B159Ad86cdB295f",
+      from: "0x26E196dbdE4d6cFA212fd5447B159Ad86cdB295f"
     },
     kovan: {
       host: "192.168.88.11",
       port: 8545,
       network_id: "*", // Match any network id
       gas: 10000000,
-      from: "0x22b2fC468FB5adca29775899d8AFFbcA414f5dE0",
+      from: "0x22b2fC468FB5adca29775899d8AFFbcA414f5dE0"
     },
     ropsten_local: {
       network_id: 3,
       host: "192.168.88.11",
       port: 8545,
       gas: 4700000,
-      from: "0x719a22e179bb49a4596efe3bd6f735b8f3b00af1",
+      from: "0x719a22e179bb49a4596efe3bd6f735b8f3b00af1"
     }
   },
   mocha: {
     useColors: true
   }
 };
-
-//
-
-
